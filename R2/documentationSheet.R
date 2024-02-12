9e24f1a312c44c473a0d11091aed40d0524242c7
nicofunblokker <nicovanblokker@gmail.com>
2024-02-12 15:47:23 +0100

    added protection to docu sheet

--- R2/documentationSheet.R
+++ R2/documentationSheet.R
@@ -24,7 +24,7 @@ addStyle(wb, sheet = sheetname, style = createStyle(halign = "left", valign = "c
 addStyle(wb, sheet = sheetname, style = createStyle(halign = "left", valign = "center", wrapText = TRUE, border = "BottomRight", borderColour = "black"), rows = 6, cols = 1:5, gridExpand = TRUE)
 
 # Write Dokumentation below the table
-text <- "Dokumentation zum Gebrauch der Notentabelle:\n– Spalten 1-2 frei editierbar. Spalten 3-5 beinhalten Formeln und sollten i.d.R nicht bearbeitet werden.\n– Die übrigen Spalten können mit Noten von 1-6 gefüllt werden (Nachkommastellen möglich).\n– Blaue Spalten zählen in die mündliche Note, orange in die schriftliche. Grau ist für nicht-stattfindende Termine vorbehalten.\n–  Um Spalten nachträglich als Klausurtermine oder Ausfall zu deklarieren, muss 'Klausur' oder 'Frei' in den jeweiligen Spaltennamen eingetragen werden. Zusätzlich muss in jedem Fall '-' im Namen auftauchen.\nSo können ebenfalls Zusatztermine hinzugefügt und kodiert werden, z.B. '2025-02-12 Klausur' (Klausurtermin) oder '2025-02-12 Frei' (Ausfall) oder '2025-02-12' (mündlich)."
+text <- "Dokumentation zum Gebrauch der Notentabelle:\n– Spalten 1-2 frei editierbar. Spalten 3-5 beinhalten Formeln und sollten i.d.R nicht bearbeitet werden.\n– Die übrigen Spalten können mit Noten von 1-6 gefüllt werden (Nachkommastellen möglich).\n– Blaue Spalten zählen in die mündliche Note, orange in die schriftliche. Grau ist für nicht-stattfindende Termine vorbehalten.\n–  Um Spalten nachträglich als Klausurtermine oder Ausfall zu deklarieren, muss 'Klausur' oder 'Frei' in den jeweiligen Spaltennamen eingetragen werden. Zusätzlich muss in jedem Fall '-' im Namen auftauchen.\n– So können ebenfalls Zusatztermine hinzugefügt und kodiert werden, z.B. '2025-02-12 Klausur' (Klausurtermin) oder '2025-02-12 Frei' (Ausfall) oder '2025-02-12' (mündlich)."
 
 showGridLines(wb, sheetname, showGridLines = FALSE)
 # Split the text by newline character
@@ -43,7 +43,8 @@ d <- data.frame("Erstellt" = text2, "Halbjahr" = halbjr, "SuS" = nsus, "Termine"
 writeData(wb, sheetname, x = d, startCol = 1, startRow = 9)
 addStyle(wb, sheet = sheetname, style = createStyle(halign = "center", valign = "center", wrapText = TRUE, border = "BottomTopRightLeft", borderColour = "black"), rows = 9:10, cols = 1:5, gridExpand = TRUE)
 
-
+# disallow editing
+protectWorksheet(wb, sheetname, protect = TRUE)
 return(wb)
 
 }
