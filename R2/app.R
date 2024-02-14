098456668b140514e40d31d5465be1c9fcd3d641
nicofunblokker <nicovanblokker@gmail.com>
2024-02-14 09:00:17 +0100

    added abwesenheit

--- R2/app.R
+++ R2/app.R
@@ -309,6 +309,24 @@ server <- function(input, output, session) {
         # disallow editing
         protectWorksheet(wb, glue::glue("Notenspiegel_HBJ{input$halbjahr}"), protect = TRUE)
 
+        # abwesenheitszeiten
+        addWorksheet(wb, glue::glue("Abwesendheit_HBJ{input$halbjahr}"))
+        Name = sprintf(glue::glue('=IFERROR({namehjr}!B%d, "")'), 2:(SuS+1))
+        Abwesend = sprintf(glue::glue('=IFERROR(COUNTIF({namehjr}!F%d:{to}%d, 0), "")'), 2:(SuS+1), 2:(SuS+1))
+        Sitzungen = sprintf(glue::glue('=IFERROR(COUNTA({namehjr}!F%d:{to}%d), "")'), 2:(SuS+1), 2:(SuS+1))
+        Prozent = sprintf(glue::glue('=IFERROR(ROUND(COUNTIF({namehjr}!F%d:{to}%d, 0) / COUNTA({namehjr}!F%d:{to}%d), 2), "")'), 2:(SuS+1), 2:(SuS+1), 2:(SuS+1), 2:(SuS+1))
+        abwesendheit <- data.frame(Name, Sitzungen, Abwesend, Prozent)
+        for(i in c(1:4)){
+          class(abwesendheit[,i]) <- c(class(abwesendheit[,i]), "formula")
+        }
+        addStyle(wb, sheet = glue::glue("Abwesendheit_HBJ{input$halbjahr}"), style = createStyle(fgFill = 'grey95', textDecoration ="bold", border = "BottomTopRightLeft", borderColour = c("grey65", "grey95","grey95","grey95" ), borderStyle = "thick"), rows = 1, cols = 1:4, gridExpand = TRUE)
+        addStyle(wb, sheet = glue::glue("Abwesendheit_HBJ{input$halbjahr}"), style = createStyle(fgFill = 'grey95'), rows = 2:(SuS+1), cols = 1:4, gridExpand = TRUE)
+        writeData(wb, glue::glue("Abwesendheit_HBJ{input$halbjahr}"), x = abwesendheit, startCol = 1, startRow = 1)
+
+        # disallow editing
+        protectWorksheet(wb, glue::glue("Abwesendheit_HBJ{input$halbjahr}"), protect = TRUE)
+        #remove gridlines
+        showGridLines(wb, glue::glue("Abwesendheit_HBJ{input$halbjahr}"), showGridLines = FALSE)
 
         # documentation
         wb <- doku(wb = wb,
